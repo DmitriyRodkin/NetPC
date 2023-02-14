264ab77d193c9c6aed7e8a8583307b2b461e3ae8
DmitriyRodkin <cozer0k@gmail.com>
2023-02-14 05:38:41 +0300

    текст коммита

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const contactButton = document.querySelector(".asdasdsa")
+const sendText = document.querySelector(".section-registration")
+const sendParagraph = document.querySelector(".registration")
+contactButton.onclick = function(){
+    sendText.classList.toggle("dddddddddd")
+    sendParagraph.classList.toggle("dddddddddd")
+}
+
+    
